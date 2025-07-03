b6e6e392f0c3641c691c9ac1421fdcf82fb3dd4f
Apurva Lavate <apurvalavate@gmail.com>
2025-07-04 02:34:45 +0530

    shortestpath.py

--- shortestpath.py
+++ shortestpath.py
@@ -1,1 +1,17 @@
 #finding a shortest path
+def find_shortest_path(graph, start, end):
+    paths = [[start]]  
+
+    while paths:
+        path = paths.pop(0)  
+        node = path[-1]      
+
+        if node == end:
+            return path  
+
+        for neighbor in graph.get(node, []):
+            if neighbor not in path:  
+                new_path = path + [neighbor]
+                paths.append(new_path)
+
+    return None  
